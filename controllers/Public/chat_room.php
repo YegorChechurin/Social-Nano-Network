0185f70ef24cbac0b4b8a0956153e5b64a3e5f31
Yegor Chechurin <yegor.chechurin@gmail.com>
2018-06-26 17:08:04 +0300

    upgrade

--- controllers/Public/chat_room.php
+++ controllers/Public/chat_room.php
@@ -3,27 +3,7 @@
     use Controllers\Pub\ChatRoom;
 
     require_once 'view_map.php';
-    //require_once 'PublicController_class.php';
     require_once 'ChatRoom_class.php';
-    /*require_once '../models/db_con.php';
-    require_once '../models/user_class.php';
-    require_once '../models/request_class.php';*/
-    
-    /*$request = new Request();
-    $user_id = $request->uri[2];
-    $user = new User($user_id,$conn);
-    $user_name = $user->showName();
-    $last_rec_mes_id = $user->getLast();
-
-    if ($request->POST) {
-        $active_id = $request->POST['fr_id'];
-        $active_name = $request->POST['fr_name'];
-    } else {
-        $active_id = 0;
-        $active_name = '';
-    }
-
-    require '../views/chat_room_view.php';*/
 
     $controller = new ChatRoom();
     $data = $controller->get_view_data($conn);
