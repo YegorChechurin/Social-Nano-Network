ee8319594070e0fd85ee2932c345326b92803ac6
Yegor Chechurin <yegor.chechurin@gmail.com>
2018-09-17 17:30:14 +0300

    upgrading

--- js/chat_room/events_map.js
+++ js/chat_room/events_map.js
@@ -1,29 +1,3 @@
-// events_map = 
-// [{
-// 	"event":"incoming_messages",
-//     "handling":[{
-//     	"handler":"MessagesHandler()",
-//     	"method":"handle_incoming_messages(data)"
-//     },
-//     {   "handler":"ChatsBarHandler()",
-//     	"method":"rearrange_chats_bar()"
-//     }]
-// },
-// {
-// 	"event":"message_sent",
-//     "handling":[{
-//     	"handler":"MessagesHandler()",
-//     	"method":"handle_sent_message(data)"
-//     },
-//     {   "handler":"ChatsBarHandler()",
-//     	"method":"rearrange_chats_bar()"
-//     }]
-// },
-// {
-// 	"event":"chat_room_load",
-// 	"handling":[{}]
-// }]
-
 events_map = 
 [{
     "event":"incoming_messages",
