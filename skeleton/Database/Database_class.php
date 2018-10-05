366f4f3d8c3404c1509a9a3b6f1de3b58a5e4970
YegorChechurin <yegor.chechurin@gmail.com>
2018-10-05 19:59:49 +0300

    adding database config file

--- skeleton/Database/Database_class.php
+++ skeleton/Database/Database_class.php
@@ -2,21 +2,25 @@
     
     namespace Skeleton\Database;
 
+    require_once('database_config.php');
+
     class Database {
         
     	private $conn;
 
     	public function __construct() {
     	    try{
-		        $dsn = "mysql:dbname=snn;host=localhost";
-		        $user="root";
-		        $password="";
+    	    	$dsn = DATABASE.':dbname='.SCHEMA.';host='.HOST;
+		        //$dsn = "mysql:dbname=snn;host=localhost";
+		        $user = USER;
+		        $password = PASSWORD;
 	            $this->conn = new PDO($dsn,$user,$password);
 		    }
 	        catch (PDOException $e){
 		        echo 'Connection failed: ' . $e->getMessage();
 	        }
     	}
+    	
     }
 
 ?>
---
+++ skeleton/Database/database_config.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace Skeleton\Database;
+
+    const DATABASE = 'mysql';
+
+    const SCHEMA = 'snn';
+
+    const HOST = 'localhost';
+
+    const USER = 'root';
+
+    const PASSWORD = '';
+
+?>
