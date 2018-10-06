68b54b3f7b824f9bb4aea834de612b4be2c79c5d
YegorChechurin <yegor.chechurin@gmail.com>
2018-10-06 20:50:22 +0300

    finalizing constructor

--- skeleton/Database/Database_class.php
+++ skeleton/Database/Database_class.php
@@ -11,14 +11,9 @@
     	public function __construct() {
     	    try{
     	    	$dsn = DATABASE.':dbname='.SCHEMA.';host='.HOST;
-		        //$dsn = "mysql:dbname=snn;host=localhost";
 		        $user = USER;
 		        $password = PASSWORD;
 	            $this->conn = new PDO($dsn,$user,$password);
-		    }
-	        catch (PDOException $e){
-		        echo 'Connection failed: ' . $e->getMessage();
-	        }
     	}
     	
     }
