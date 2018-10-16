9a63fef588c0e01579b2d9c2294b895347092969
YegorChechurin <yegor.chechurin@gmail.com>
2018-10-17 00:38:40 +0300

    adding insert method

--- skeleton/Database/Database.php
+++ skeleton/Database/Database.php
@@ -2,20 +2,32 @@
     
     namespace Skeleton\Database;
 
-    echo $GLOBALS['USER'];
-
     require_once('database_config.php');
 
     class Database {
         
-    	public $conn;
+    	private $conn;
 
     	public function __construct() {
-    	    $dsn = DATABASE.':dbname='.SCHEMA.';host='.HOST;
-		    $user = USER;
-		    $password = PASSWORD;
-	        $this->conn = new \PDO($dsn,$user,$password);
+	        $this->conn = new \PDO(DSN,USER,PASSWORD);
     	}
+
+        public function insert($table,$fields,$values) {
+            $format = 'INSERT INTO %s (%s) VALUES (:%s)';
+            $names = implode(',',$fields);
+            $params = implode(',:',$fields);
+            $query = sprintf($format,$table,$names,$params);
+            $prep = $this->conn->prepare($query);
+            $n = count($fields);
+            for ($i=0; $i < $n; $i++) { 
+                $prep->bindParam(':'.$fields[$i], $values[$i]);
+            }
+            $prep->execute();
+        }
+
+        public function select() {}
+
+        public function update() {}
 
     }
 
