58d84c4663506ac8893d2ed979344995c5060761
YegorChechurin <yegor.chechurin@gmail.com>
2018-10-27 20:20:50 +0300

    new methods added

--- skeleton/Database/Database.php
+++ skeleton/Database/Database.php
@@ -2,8 +2,6 @@
     
     namespace Skeleton\Database;
 
-    require_once('database_config.php');
-
     class Database {
         
     	private $conn;
@@ -12,7 +10,7 @@
 	        $this->conn = new \PDO(DSN,USER,PASSWORD);
     	}
 
-        public function insert($table,$fields,$values) {
+        public function insert($table, Array $fields, Array $values) {
             $format = 'INSERT INTO %s (%s) VALUES (:%s)';
             $names = implode(',',$fields);
             $params = implode(',:',$fields);
@@ -25,9 +23,33 @@
             $prep->execute();
         }
 
-        public function select() {}
+        public function select($table, Array $fields, $clause_exp, Array $clause_pars) {
+            $format = "SELECT %s FROM %s WHERE {$clause_exp}";
+            $fields = implode(',',$fields);
+            $query = sprintf($format,$fields,$table);
+            $prep = $this->conn->prepare($query);
+            foreach ($clause_pars as $key => $value) {
+                $prep->bindParam($key, $value);
+            }
+            $prep->execute();
+            $result = $prep->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        }
 
-        public function update() {}
+        public function update($table, Array $fields, $clause, Array $parameter_map) {
+            $format = "UPDATE %s SET %s WHERE {$clause}";
+            $param_fields = [];
+            foreach ($fields as $field) {
+                $param_fields[] = $field.'=:'.$field;
+            }
+            $structure = implode(',',$param_fields);
+            $query = sprintf($format,$table,$structure);
+            $prep = $this->conn->prepare($query);
+            foreach ($parameter_map as $key => $value) {
+                $prep->bindValue($key, $value);
+            }
+            $result = $prep->execute();
+        }
 
     }
 
