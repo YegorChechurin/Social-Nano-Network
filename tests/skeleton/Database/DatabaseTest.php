9bea8bec93f80a67468f566095620f3a0de5d0d3
YegorChechurin <yegor.chechurin@gmail.com>
2018-11-01 19:27:17 +0200

    changes made related to the fact that database table which is used for unit tests of Skeleton\Database\Database, has been renamed from unit_tests to database_tests

--- tests/skeleton/Database/DatabaseTest.php
+++ tests/skeleton/Database/DatabaseTest.php
@@ -12,6 +12,8 @@ class DatabaseTest extends TestCase {
 
     use TestCaseTrait;
 
+    private $table = 'database_tests';
+
     public function getConnection() {   
         $pdo = new PDO(DSN,USER,PASSWORD);
         $conn = $this->createDefaultDBConnection($pdo, SCHEMA);
@@ -19,26 +21,24 @@ class DatabaseTest extends TestCase {
     }
 
     public function getDataSet() {
-        return $this->createFlatXMLDataSet(dirname(__FILE__).'/unit_tests-seed.xml');
+        return $this->createFlatXMLDataSet(dirname(__FILE__).'/DatabaseFixture.xml');
     }
 
     public function testInsert() {
-        $table = 'unit_tests';
         $fields = ['id','content','user','created'];
         $values = [3,'test','yegor','now'];
         $db = new Database();
-        $db->insert($table,$fields,$values);
-        $queryTable = $this->getConnection()->createQueryTable($table,"SELECT * FROM {$table}");
+        $db->insert($this->table,$fields,$values);
+        $queryTable = $this->getConnection()->createQueryTable($this->table,"SELECT * FROM {$this->table}");
         $expectedTable = $this->createFlatXmlDataSet(dirname(__FILE__).'/expectedInsert.xml')
-                              ->getTable($table);
+                              ->getTable($this->table);
         $this->assertTablesEqual($expectedTable, $queryTable);
     }
 
     public function testSelect() {
-        $table = 'unit_tests';
         $fields = ['*'];
         $db = new Database();
-        $outcome = $db->select($table,$fields);
+        $outcome = $db->select($this->table,$fields);
         $expected_outcome = [
             ['id'=>1, 'content'=>'Hello buddy!', 'user'=>'joe', 'created'=>'2010-04-24 17:15:23'],
             ['id'=>2, 'content'=>'I like it!', 'user'=>'nancy', 'created'=>'2010-04-26 12:14:20']
@@ -47,20 +47,19 @@ class DatabaseTest extends TestCase {
         $fields = ['content'];
         $clause = 'id=:id';
         $clause_pars = [':id'=>2];
-        $crude_outcome = $db->select($table,$fields,$clause,$clause_pars);
+        $crude_outcome = $db->select($this->table,$fields,$clause,$clause_pars);
         $outcome = $crude_outcome[0]['content'];
         $expected_outcome = 'I like it!';
         $this->assertTrue($outcome==$expected_outcome);
         $clause = 'user=:user';
         $clause_pars = [':user'=>'nancy'];
-        $crude_outcome = $db->select($table,$fields,$clause,$clause_pars);
+        $crude_outcome = $db->select($this->table,$fields,$clause,$clause_pars);
         $outcome = $crude_outcome[0]['content'];
         $expected_outcome = 'I like it!';
         $this->assertTrue($outcome==$expected_outcome);
     }
 
     public function testUpdate() {
-        $table = 'unit_tests';
         $fields = ['content','created'];
         $clause = 'id=:id AND user=:user';
         $map = [
@@ -68,10 +67,10 @@ class DatabaseTest extends TestCase {
             ':created'=>'right now'
         ];
         $db = new Database();
-        $db->update($table,$fields,$clause,$map);
-        $queryTable = $this->getConnection()->createQueryTable($table,"SELECT * FROM {$table}");
+        $db->update($this->table,$fields,$clause,$map);
+        $queryTable = $this->getConnection()->createQueryTable($this->table,"SELECT * FROM {$this->table}");
         $expectedTable = $this->createFlatXmlDataSet(dirname(__FILE__).'/expectedUpdate.xml')
-                              ->getTable($table);
+                              ->getTable($this->table);
         $this->assertTablesEqual($expectedTable, $queryTable);
     }
 
