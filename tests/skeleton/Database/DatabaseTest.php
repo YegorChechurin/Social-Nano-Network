a80034513a769640670a35f88c870b80f939e3fc
YegorChechurin <yegor.chechurin@gmail.com>
2018-11-08 16:30:11 +0200

    upgraded: multiple database connections issue resolved

--- tests/skeleton/Database/DatabaseTest.php
+++ tests/skeleton/Database/DatabaseTest.php
@@ -14,10 +14,15 @@ class DatabaseTest extends TestCase {
 
     private $table = 'database_tests';
 
-    public function getConnection() {   
+    private $conn;
+
+    private $db;
+
+    public function getConnection() { 
+        $this->db = new Database();  
         $pdo = new PDO(DSN,USER,PASSWORD);
-        $conn = $this->createDefaultDBConnection($pdo, SCHEMA);
-        return $conn;
+        $this->conn = $this->createDefaultDBConnection($pdo, SCHEMA);
+        return $this->conn;
     }
 
     public function getDataSet() {
@@ -27,9 +32,9 @@ class DatabaseTest extends TestCase {
     public function testInsert() {
         $fields = ['id','content','user','created'];
         $values = [3,'test','yegor','now'];
-        $db = new Database();
-        $db->insert($this->table,$fields,$values);
-        $queryTable = $this->getConnection()->createQueryTable($this->table,"SELECT * FROM {$this->table}");
+        //$db = new Database();
+        $this->db->insert($this->table,$fields,$values);
+        $queryTable = $this->conn->createQueryTable($this->table,"SELECT * FROM {$this->table}");
         $expectedTable = $this->createFlatXmlDataSet(dirname(__FILE__).'/expectedInsert.xml')
                               ->getTable($this->table);
         $this->assertTablesEqual($expectedTable, $queryTable);
@@ -37,8 +42,8 @@ class DatabaseTest extends TestCase {
 
     public function testSelect() {
         $fields = ['*'];
-        $db = new Database();
-        $outcome = $db->select($this->table,$fields);
+        //$db = new Database();
+        $outcome = $this->db->select($this->table,$fields);
         $expected_outcome = [
             ['id'=>1, 'content'=>'Hello buddy!', 'user'=>'joe', 'created'=>'2010-04-24 17:15:23'],
             ['id'=>2, 'content'=>'I like it!', 'user'=>'nancy', 'created'=>'2010-04-26 12:14:20']
@@ -47,13 +52,13 @@ class DatabaseTest extends TestCase {
         $fields = ['content'];
         $clause = 'id=:id';
         $clause_pars = [':id'=>2];
-        $crude_outcome = $db->select($this->table,$fields,$clause,$clause_pars);
+        $crude_outcome = $this->db->select($this->table,$fields,$clause,$clause_pars);
         $outcome = $crude_outcome[0]['content'];
         $expected_outcome = 'I like it!';
         $this->assertTrue($outcome==$expected_outcome);
         $clause = 'user=:user AND id=:id';
         $clause_pars = [':user'=>'nancy',':id'=>2];
-        $crude_outcome = $db->select($this->table,$fields,$clause,$clause_pars);
+        $crude_outcome = $this->db->select($this->table,$fields,$clause,$clause_pars);
         $outcome = $crude_outcome[0]['content'];
         $expected_outcome = 'I like it!';
         $this->assertTrue($outcome==$expected_outcome);
@@ -66,9 +71,9 @@ class DatabaseTest extends TestCase {
             ':id'=>1, ':user'=>'joe', ':content'=>'UPDATED', 
             ':created'=>'right now'
         ];
-        $db = new Database();
-        $db->update($this->table,$fields,$clause,$map);
-        $queryTable = $this->getConnection()->createQueryTable($this->table,"SELECT * FROM {$this->table}");
+        //$db = new Database();
+        $this->db->update($this->table,$fields,$clause,$map);
+        $queryTable = $this->conn->createQueryTable($this->table,"SELECT * FROM {$this->table}");
         $expectedTable = $this->createFlatXmlDataSet(dirname(__FILE__).'/expectedUpdate.xml')
                               ->getTable($this->table);
         $this->assertTablesEqual($expectedTable, $queryTable);
@@ -77,9 +82,9 @@ class DatabaseTest extends TestCase {
     public function testDelete() {
         $clause = 'id=:id AND user=:user';
         $map = [':id'=>1, ':user'=>'joe'];
-        $db = new Database();
-        $db->delete($this->table,$clause,$map);
-        $queryTable = $this->getConnection()->createQueryTable($this->table,"SELECT * FROM {$this->table}");
+        //$db = new Database();
+        $this->db->delete($this->table,$clause,$map);
+        $queryTable = $this->conn->createQueryTable($this->table,"SELECT * FROM {$this->table}");
         $expectedTable = $this->createFlatXmlDataSet(dirname(__FILE__).'/expectedDelete.xml')
                               ->getTable($this->table);
         $this->assertTablesEqual($expectedTable, $queryTable);
