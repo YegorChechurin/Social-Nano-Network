bf250b8e30ad4a40570bab2bba378bb1ae228555
YegorChechurin <yegor.chechurin@gmail.com>
2018-11-15 20:27:20 +0200

    User_class.php renamed to User.php

--- models/User.php
+++ models/User.php
@@ -4,7 +4,7 @@
 
     class User {
 	   
-	   protected $conn;
+	   protected $DB;
 	   protected $id;	   
 	   protected $name;
 	   protected $fr_IDs;
@@ -12,9 +12,9 @@
        protected $last_friendship_id;
        protected $chats;
 	   
-	   public function __construct($us_id,$conn) {
+	   public function __construct($us_id,$database) {
 		   $this->id = $us_id;
-		   $this->conn = $conn;
+		   $this->DB = $database;
            $this->name = $this->fetchName();
  	   }
 	   
@@ -23,18 +23,19 @@
 	   }
 
 	   public function fetchName() {
-	   	   $query = "SELECT username FROM users WHERE user_id=$this->id";
-		   $action = $this->conn->query($query);
-		   $result = $action->fetch(\PDO::FETCH_ASSOC);
-		   $this->name = $result['username'];
-		   return $this->name;
+		   $table = 'users';
+		   $fields = ['username'];
+		   $clause = 'user_id=:id';
+		   $map = [':id'=>$this->id];
+		   $result = $this->DB->select($table,$fields,$clause,$map);
+		   $this->name = $result[0]['username'];
 	   }
 	   
 	   public function showName() {
 		   return $this->name;
 	   }
 
-	   public function get_fr_IDs() {
+	   /* public function get_fr_IDs() {
            $stat = "SELECT fr2_id FROM friends WHERE fr1_id=$this->id";
            $impl = $this->conn->query($stat);
            $res = $impl->fetchAll(\PDO::FETCH_ASSOC);
@@ -84,52 +85,7 @@
 		   	  return 0;
 		   }
 		   
-	   }
-
-	   public function fetch_chats() {
-	   	   $query = "SELECT * FROM chats WHERE participant1_id=$this->id OR participant2_id=$this->id ORDER BY	last_mes_ts DESC";
-	   	   $result = $this->conn->query($query);
-	   	   $chats_raw = $result->fetchAll(\PDO::FETCH_ASSOC);
-	   	   if ($chats_raw) {
-	   	   	  foreach ($chats_raw as $key => $value) {
-	   	   	  	$chat['partner_id'] = $value['chat_key'] - $this->id;
-	   	   	  	if ($value['participant1_id']==$this->id) {
-	   	   	  		$chat['partner_name'] = $value['participant2_name'];
-	   	   	  	} else {
-	   	   	  		$chat['partner_name'] = $value['participant1_name'];
-	   	   	  	}
-	   	   	  	$chat['last_mes_auth_id'] = $value['last_mes_auth_id'];
-	   	   	  	$chat['last_mes_auth_name'] = $value['last_mes_auth_name'];
-	   	   	  	$chat['last_mes_text'] = $value['last_mes_text'];
-	   	   	  	$chat['last_mes_ts'] = $value['last_mes_ts'];
-	   	   	  	$chats[] = $chat;
-	   	   	  }
-	   	   	  return json_encode($chats);
-	   	   } else {
-	   	   	  return 0;
-	   	   }
-	   }
-
-	   public function getLast() {
-		   $query = "SELECT MAX(message_id) FROM messages WHERE recipient_id = $this->id";
-		   $result = $this->conn->query($query);
-		   $maximum = $result->fetch(\PDO::FETCH_ASSOC);
-		   $mes_id = $maximum['MAX(message_id)'];
-		   if ($maximum) {
-		   	  $mes_id = $maximum['MAX(message_id)'];
-		      return $mes_id;
-		   } else {
-		   	  return 0;
-		   }
-	   }
-
-	   public function fetch_received_messages($message_id) {
-	       $test = $message_id;
-	       $query = "SELECT * FROM messages WHERE recipient_id = $this->id AND message_id > $test ORDER BY message_id";
-	       $result = $this->conn->query($query);
-		   $messages = $result->fetchAll(\PDO::FETCH_ASSOC);	
-		   return $messages;
-	   }
+	   } */
 	   
     }
 ?>
