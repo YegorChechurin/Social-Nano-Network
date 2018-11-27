9b825d5a03e82a9e5c24b49d1771105aabcccced
YegorChechurin <yegor.chechurin@gmail.com>
2018-11-27 17:17:40 +0200

    a factory for initializing Messenger and FriendBroker classes created

---
+++ models/ServiceFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+    namespace Models;
+    use Skeleton\Database\Database;
+
+    class ServiceFactory {
+
+        private $database;
+
+        public function __construct(Database $database) {
+        	$this->database = $database;
+        }
+
+        public function make_service_instance($service_name) {
+        	if ($service_name=='Messenger') {
+        		return new Messenger($this->database);
+        	} elseif ($service_name=='FriendBroker') {
+        		return new FriendBroker($this->database);
+        	}
+        }
+
+    }
+
+?>
