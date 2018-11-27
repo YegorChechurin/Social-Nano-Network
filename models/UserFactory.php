6694a71a226ce5043af7eee4b3bfa9d9f7176e78
YegorChechurin <yegor.chechurin@gmail.com>
2018-11-27 17:20:38 +0200

    a factory for initializing User class created

---
+++ models/UserFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+    namespace Models;
+    use Skeleton\Database\Database;
+
+    class UserFactory {
+
+        private $database;
+
+        public function __construct(Database $database) {
+        	$this->database = $database;
+        }
+
+        public function make_user($user_id) {
+        	return new User($user_id,$this->database);
+        }
+
+    }
+
+?>
