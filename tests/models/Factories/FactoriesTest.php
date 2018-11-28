9fddedd5f70af307dd7e98b00a62b9be4b981a4f
YegorChechurin <yegor.chechurin@gmail.com>
2018-11-29 00:30:39 +0200

    unit tests for testing Models\UserFactory and Models\ServiceFactory created

---
+++ tests/models/Factories/FactoriesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Skeleton\Database\Database;
+use Models\User;
+use Models\Messenger;
+use Models\FriendBroker;
+use Models\UserFactory;
+use Models\ServiceFactory;
+use const Skeleton\Database\DSN;
+use const Skeleton\Database\USER;
+use const Skeleton\Database\PASSWORD;
+use const Skeleton\Database\SCHEMA;
+
+class FactoriesTest extends TestCase {
+
+	public function testUserFactory() {
+		$db = new Database();
+		$factory = new UserFactory($db);
+		$user_id = 1;
+		$user = $factory->make_user($user_id);
+		$this->assertInstanceOf('Models\User',$user);
+	}
+
+    public function testServiceFactory() {
+    	$db = new Database();
+    	$factory = new ServiceFactory($db);
+    	$service = 'Messenger';
+    	$messenger = $factory->make_service_instance($service);
+    	$this->assertInstanceOf('Models\Messenger',$messenger);
+    	$service = 'FriendBroker';
+    	$messenger = $factory->make_service_instance($service);
+    	$this->assertInstanceOf('Models\FriendBroker',$messenger);
+    }
+
+}
+
+?>
