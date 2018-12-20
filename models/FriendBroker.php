f51dd8ecf76861b2420279290ae2b90eeb213c8a
YegorChechurin <yegor.chechurin@gmail.com>
2018-12-20 06:36:30 +0200

    added method delete_friendship

--- models/FriendBroker.php
+++ models/FriendBroker.php
@@ -30,8 +30,23 @@
             $this->table = 'friends';
     	}
 
+        /**
+         * Establishes new friendship connection
+         *
+         * First checks whether friendship between the users has been already 
+         * established. If not posts user IDs and names to the appropriate 
+         * database table, thus recording that the users are friends.
+         *
+         * @param integer $id_1 - User id of the user who is initiating the 
+         * friendship (clicking the add to friends button).
+         * @param integer $id_2 - User id of the user who has been added to 
+         * friends.
+         * @param string $name_1 - User name of the user who is initiating the 
+         * friendship (clicking the add to friends button).
+         * @param string $name_2 - User name of the user who has been added to 
+         * friends.
+         */
         public function make_friendship($id_1,$id_2,$name_1,$name_2) {
-            // check whether the friendship already exists, if not, post a new friendship
             $fields = ['friendship_id'];
             $clause = '(friend1_id=:id_1 AND friend2_id=:id_2) OR (friend1_id=:id_2 AND friend2_id=:id_1)';
             $map = [
@@ -49,10 +64,45 @@
             } 
         }
 
-        public function fetch_all_friends($user_id) {
-            /*$fields = ['users.user_id','users.username'];
-            $clause = 'INNER JOIN users ON users.user_id=friends.fr_id WHERE friends.friendship_id=array_containing_list_of_IDs';*/
+        /**
+         * Deletes friendship between users
+         *
+         * First checks whether friendship between the users already exists. If
+         * it does, deletes corresponding record from the appropriate table, thus
+         * removing the friendship.
+         *
+         * @param integer $id_1 - User id of the user who has initiated the 
+         * friendship (clicked the add to friends button).
+         * @param integer $id_2 - User id of the user who has been added to 
+         * friends.
+         */
+        public function delete_friendship($id_1,$id_2) {
+            $fields = ['friendship_id'];
+            $clause = '(friend1_id=:id_1 AND friend2_id=:id_2) OR (friend1_id=:id_2 AND friend2_id=:id_1)';
+            $map = [
+               ':id_1'=>$id_1,
+               ':id_2'=>$id_2
+            ];
+            $friendship_id = $this->DB->select($this->table,$fields,$clause,$map); 
+            if ($friendship_id) {
+                $this->DB->delete($this->table,$clause,$map);
+            } 
+        }
 
+        /**
+         * Fetches information about all the friends that a specific user has
+         *
+         * If a user has friends, fetches information about every friendship 
+         * connection, namely friendship id, friend user id and name.
+         *
+         * @param integer $user_id - User id of the user for whom information about
+         * friendship connections is fetched.
+         *
+         * @return integer|string[] - Zero if user has no friends, otherwise array 
+         * of strings which contains information about every friendship connection
+         * the user has, namely friendship id, friend user id and name. 
+         */
+        public function fetch_all_friends($user_id) {
             $fields = [
                     'friend1_id','friend2_id',
                     'friend1_name','friend2_name'
@@ -60,21 +110,44 @@
             $clause = 'friend1_id=:id OR friend2_id=:id';
             $map = [':id'=>$user_id];
             $all_info = $this->DB->select($this->table,$fields,$clause,$map);
-            $friend_info = [];
-            $n = 0;
-            foreach ($all_info as $info_unit) {
-                if ($info_unit['friend1_id'] == $user_id) {
-                    $friend_info[$n]['friend_id'] = $info_unit['friend2_id'];
-                    $friend_info[$n]['friend_name'] = $info_unit['friend2_name'];
-                } else {
-                    $friend_info[$n]['friend_id'] = $info_unit['friend1_id'];
-                    $friend_info[$n]['friend_name'] = $info_unit['friend1_name'];
+            if ($all_info) {
+                $friend_info = [];
+                $n = 0;
+                foreach ($all_info as $info_unit) {
+                    if ($info_unit['friend1_id'] == $user_id) {
+                        $friend_info[$n]['friend_id'] = $info_unit['friend2_id'];
+                        $friend_info[$n]['friend_name'] = $info_unit['friend2_name'];
+                    } else {
+                        $friend_info[$n]['friend_id'] = $info_unit['friend1_id'];
+                        $friend_info[$n]['friend_name'] = $info_unit['friend1_name'];
+                    }
+                    $n++;
                 }
-                $n++;
+                return $friend_info;
+            } else {
+                return 0;
             }
-            return $friend_info;
         }
 
+        /**
+         * Fetches information about user friendship connections whose 
+         * friendship id is greater than the provided friendship id
+         *
+         * If user has friendship connections with friendship id greater
+         * than the provided friendship id, fetches information about all of
+         * these friendship connections, namely friendship id, friend user id 
+         * and name.
+         *
+         * @param integer $user_id - User id of the user for whom information about
+         * friendship connections is fetched.
+         * @param integer $friendship_id - Friendship id value greater which 
+         * friendship connections are fetched. 
+         *
+         * @return integer|string[] - Zero if user has no friendship connections
+         * with friendship id greater than the provided friendship id, otherwise 
+         * array of strings which contains information about all of these 
+         * friendship connections, namely friendship id, friend user id and name. 
+         */
         public function fetch_new_friends($user_id,$friendship_id) {
             $fields = ['*'];
             $clause = '(friend1_id=:u_id OR friend2_id=:u_id) AND friendship_id>:f_id';
