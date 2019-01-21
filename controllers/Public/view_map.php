c6b6bd6df09a37bd84f2ad77ace6a4550c9bfdac
YegorChechurin <yegor.chechurin@gmail.com>
2019-01-21 14:08:13 +0200

    new view added

--- controllers/Public/view_map.php
+++ controllers/Public/view_map.php
@@ -4,6 +4,9 @@
     	array('controller'=>'Controllers\Pub\ChatRoom',
     		'view'=>'../views/chat_room_view.php'
         ),
+        array('controller'=>'Controllers\Pub\ProfilePage',
+    		'view'=>'../views/profile_page_view.php'
+        )
     );
 
 ?>
