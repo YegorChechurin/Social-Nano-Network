31b2a2ed35689a1cc9b299e7ddf81ba429d8b89c
YegorChechurin <yegor.chechurin@gmail.com>
2019-03-12 15:23:33 +0200

    observer pattern implemented

--- controllers/Ajax/friend_remover.php
+++ controllers/Ajax/friend_remover.php
@@ -3,10 +3,13 @@
     use Skeleton\RequestHandling\Request;
     use Skeleton\Database\Database;
     use Models\FriendBroker;
+    use Models\Messenger;
     
     $request = new Request();
     $user_id = $request->uri[2];
     $friend_id = $request->uri[4];
     $db = new Database();
     $friend_broker = new FriendBroker($db);
+    $messenger = new Messenger($db);
+    $friend_broker->attach_observer($messenger,'friendship_deleted');
     $friend_broker->delete_friendship($user_id,$friend_id);
