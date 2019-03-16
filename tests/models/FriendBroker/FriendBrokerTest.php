5b826406c79cb9cab77cf4de88f357f544acc4c2
YegorChechurin <yegor.chechurin@gmail.com>
2019-03-16 14:45:03 +0200

    tests for friend-tracking methods added

--- tests/models/FriendBroker/FriendBrokerTest.php
+++ tests/models/FriendBroker/FriendBrokerTest.php
@@ -104,6 +104,7 @@ class FriendBrokerTest extends TestCase {
         $actual_dataset = $this->conn->createDataset($this->tables);
         $expected_dataset = $this->createFlatXMLDataSet(dirname(__FILE__).'/expectedDeleteFriendship.xml');
         $this->assertDataSetsEqual($expected_dataset,$actual_dataset);
+        $this->FB->delete_friendship(1,2);
     }
 
     public function testFetchFriendshipID() {
@@ -112,19 +113,50 @@ class FriendBrokerTest extends TestCase {
         $expected_friendship_id = '3';
         $friendship_id = $this->FB->fetch_friendship_id($id_1,$id_2);
         $this->assertEquals($friendship_id,$expected_friendship_id); 
+        // Testing exception
+        $id_1 = 100;
+        $id_2 = 500;
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("No friendship id can be fetched, as no friendship between users with user IDs 100 and 500 exist");
+        $friendship_id = $this->FB->fetch_friendship_id($id_1,$id_2);
     }
 
     public function testFetchAllFriends() {
     	$user_id = 1;
     	$friend_info = $this->FB->fetch_all_friends($user_id);
-    	$expected_result = json_encode(
+    	$expected_result =
             [
                 ['friendship_id'=>'1','friend_id'=>'2','friend_name'=>'User2'],
                 ['friendship_id'=>'2','friend_id'=>'4','friend_name'=>'Test1'],
                 ['friendship_id'=>'3','friend_id'=>'5','friend_name'=>'Test2']
-            ]
-        );
+            ];
     	$this->assertEquals($friend_info,$expected_result);
+    }
+
+    public function testFetchSpecificFriends() {
+        $user_id = 1;
+        $friendship_IDs = ['2','3'];
+        $friend_records = $this->FB->fetch_specific_friends($user_id,$friendship_IDs);
+        $expected_result = [
+            ['friendship_id'=>'2','friend_id'=>'4','friend_name'=>'Test1'],
+            ['friendship_id'=>'3','friend_id'=>'5','friend_name'=>'Test2']
+        ];
+        $this->assertEquals($friend_records,$expected_result);
+        $friendship_IDs = ['2'];
+        $friend_records = $this->FB->fetch_specific_friends($user_id,$friendship_IDs);
+        $expected_result = [
+            ['friendship_id'=>'2','friend_id'=>'4','friend_name'=>'Test1']
+        ];
+        $this->assertEquals($friend_records,$expected_result);
+        // Testing exceptions
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("No friendship_IDs provided");
+        $friendship_IDs = [];
+        $friend_records = $this->FB->fetch_specific_friends($user_id,$friendship_IDs);
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("No friendship records can be fetched for provided friendship IDs");
+        $friendship_IDs = ['4','5','6'];
+        $friend_records = $this->FB->fetch_specific_friends($user_id,$friendship_IDs);
     }
 
     public function testFetchNewFriends() {
@@ -141,7 +173,7 @@ class FriendBrokerTest extends TestCase {
     	$user_id = 1;
     	$friendship_id = 3;
     	$outcome = $this->FB->fetch_new_friends($user_id,$friendship_id);
-        $expected_raw = [
+        $expected = [
             [
             	'friendship_id'=>'4','friend_id'=>'3','friend_name'=>'User3'
             ],
@@ -149,7 +181,6 @@ class FriendBrokerTest extends TestCase {
             	'friendship_id'=>'5','friend_id'=>'6','friend_name'=>'Test3'
             ]
         ];
-        $expected = json_encode($expected_raw);
         $this->assertEquals($expected,$outcome);
     }
 
@@ -158,6 +189,150 @@ class FriendBrokerTest extends TestCase {
         $outcome = $this->FB->fetch_last_friendship_id($user_id);
         $expected = 3;
         $this->assertTrue($expected==$outcome);
+    }
+
+    public function testFetchFriendshipIDs() {
+        $user_id = 1;
+        $outcome = $this->FB->fetch_friendship_IDs($user_id);
+        $expected = ['1','2','3'];
+        $this->assertEquals($expected,$outcome);
+    }
+
+    public function testProcessFriendChange() {
+        $user_id = 1;
+        $real_friendship_IDs = $this->FB->fetch_friendship_IDs($user_id);
+
+        // Scenario: no lost friends, one new friend obtained
+        $friendship_IDs = ['1','2'];
+        $outcome = $this->FB->process_friend_change($user_id,$friendship_IDs,$real_friendship_IDs);
+        $expected_raw = [
+                    'friends_obtained'=>'yes',
+                    'friends_lost'=>'no',
+                    'lost_friendship_IDs'=>0,
+                    'new_friends'=>[
+                        ['friendship_id'=>'3',
+                        'friend_id'=>'5',
+                        'friend_name'=>'Test2']
+                    ],
+                    'friendship_IDs'=>$real_friendship_IDs
+                ];
+        $expected = json_encode($expected_raw);
+        $this->assertEquals($expected,$outcome);
+
+        // Scenario: no lost friends, two new friend obtained
+        $friendship_IDs = ['2'];
+        $outcome = $this->FB->process_friend_change($user_id,$friendship_IDs,$real_friendship_IDs);
+        $expected_raw = [
+                    'friends_obtained'=>'yes',
+                    'friends_lost'=>'no',
+                    'lost_friendship_IDs'=>0,
+                    'new_friends'=>[
+                        ['friendship_id'=>'1',
+                        'friend_id'=>'2',
+                        'friend_name'=>'User2'],
+                        ['friendship_id'=>'3',
+                        'friend_id'=>'5',
+                        'friend_name'=>'Test2']
+                    ],
+                    'friendship_IDs'=>$real_friendship_IDs
+                ];
+        $expected = json_encode($expected_raw);
+        $this->assertEquals($expected,$outcome);
+
+        // Scenario: friend list has been totally renewed - all friends are new, all previous friends are lost
+        $friendship_IDs = ['10','15','-47'];
+        $outcome = $this->FB->process_friend_change($user_id,$friendship_IDs,$real_friendship_IDs);
+        $expected_raw = [
+                    'friends_obtained'=>'all_new',
+                    'friends_lost'=>'all_lost',
+                    'lost_friendship_IDs'=>$friendship_IDs,
+                    'new_friends'=>[
+                        ['friendship_id'=>'1',
+                        'friend_id'=>'2',
+                        'friend_name'=>'User2'],
+                        ['friendship_id'=>'2',
+                        'friend_id'=>'4',
+                        'friend_name'=>'Test1'],
+                        ['friendship_id'=>'3',
+                        'friend_id'=>'5',
+                        'friend_name'=>'Test2']
+                    ],
+                    'friendship_IDs'=>$real_friendship_IDs
+                ];
+        $expected = json_encode($expected_raw);
+        $this->assertEquals($expected,$outcome);
+
+        // Scenario: no friends were obtained, one friend was lost
+        $friendship_IDs = ['1','2','3','4'];
+        $outcome = $this->FB->process_friend_change($user_id,$friendship_IDs,$real_friendship_IDs);
+        $expected_raw = [
+                    'friends_obtained'=>'no',
+                    'friends_lost'=>'yes',
+                    'lost_friendship_IDs'=>['4'],
+                    'new_friends'=>0,
+                    'friendship_IDs'=>$real_friendship_IDs
+                ];
+        $expected = json_encode($expected_raw);
+        $this->assertEquals($expected,$outcome);
+
+        // Scenario: one friend was obtained, one friend was lost
+        $friendship_IDs = ['2','3','4'];
+        $outcome = $this->FB->process_friend_change($user_id,$friendship_IDs,$real_friendship_IDs);
+        $expected_raw = [
+                    'friends_obtained'=>'yes',
+                    'friends_lost'=>'yes',
+                    'lost_friendship_IDs'=>['4'],
+                    'new_friends'=>[
+                        ['friendship_id'=>'1',
+                        'friend_id'=>'2',
+                        'friend_name'=>'User2']
+                    ],
+                    'friendship_IDs'=>$real_friendship_IDs
+                ];
+        $expected = json_encode($expected_raw);
+        $this->assertEquals($expected,$outcome);
+    }
+
+    public function testProcessExtremeFriendChange() {
+        $user_id = 1;
+
+        // Scenario: friend list has been totally renewed - no new friends, all previous friends are lost
+        $real_friendship_IDs = false;
+        $friendship_IDs = ['1','2','3'];
+        $outcome = $this->FB->process_extreme_friend_change($user_id,$friendship_IDs,$real_friendship_IDs);
+        $expected_raw = [
+                    'friends_obtained'=>'no',
+                    'friends_lost'=>'all_lost',
+                    'lost_friendship_IDs'=>$friendship_IDs,
+                    'new_friends'=>0,
+                    'friendship_IDs'=>0
+                ];
+        $expected = json_encode($expected_raw);
+        $this->assertEquals($expected,$outcome);
+
+        // Scenario: friend list has been totally renewed - all friends are new, because previously user had no friends at all
+        $real_friendship_IDs = ['1','2','3'];
+        $friendship_IDs = 0;
+        $outcome = $this->FB->process_extreme_friend_change($user_id,$friendship_IDs,$real_friendship_IDs);
+        $expected_raw = [
+                    'friends_obtained'=>'all_new',
+                    'friends_lost'=>'no',
+                    'lost_friendship_IDs'=>$friendship_IDs,
+                    'new_friends'=>[
+                        ['friendship_id'=>'1',
+                        'friend_id'=>'2',
+                        'friend_name'=>'User2'],
+                        ['friendship_id'=>'2',
+                        'friend_id'=>'4',
+                        'friend_name'=>'Test1'],
+                        ['friendship_id'=>'3',
+                        'friend_id'=>'5',
+                        'friend_name'=>'Test2']
+                    ],
+                    'friendship_IDs'=>$real_friendship_IDs
+                ];
+        $expected = json_encode($expected_raw);
+        $this->assertEquals($expected,$outcome);
     }
 
 }
