cb8504073842fd702c52fc3b881f3edfe0ea44ff
YegorChechurin <yegor.chechurin@gmail.com>
2019-05-15 19:54:11 +0300

    bug fixed

--- js/chat_room/FriendsHandler.js
+++ js/chat_room/FriendsHandler.js
@@ -15,18 +15,32 @@ function FriendsHandler(event_handler) {
 	}
 
 	this.handle_friend_data = function(friend_data){
-		if (friend_data.friends_lost=='yes' && friend_data.friends_obtained=='yes') {
-			var lost_friends_handler = mediator.create_object(LostFriendsHandler);
-			var new_friends_handler = mediator.create_object(NewFriendsHandler);
-			lost_friends_handler.handle_friend_data(friend_data);
-			new_friends_handler.handle_friend_data(friend_data);
-		} else if (friend_data.friends_lost=='no' && friend_data.friends_obtained=='yes') {
-			var new_friends_handler = mediator.create_object(NewFriendsHandler);
-			new_friends_handler.handle_friend_data(friend_data);
-		} else if (friend_data.friends_lost=='yes' && friend_data.friends_obtained=='no') {
-			var lost_friends_handler = mediator.create_object(LostFriendsHandler);
-			lost_friends_handler.handle_friend_data(friend_data);
-		}
-	}
+        if (JSON.stringify(friendship_IDs)!=JSON.stringify(friend_data.friendship_IDs)) {
+            if (
+                (friend_data.friends_lost=='yes' || friend_data.friends_lost=='all_lost') 
+                && 
+                (friend_data.friends_obtained=='yes' || friend_data.friends_obtained=='all_new')
+                ) 
+            {
+                var lost_friends_handler = mediator.create_object(LostFriendsHandler);
+                var new_friends_handler = mediator.create_object(NewFriendsHandler);
+                lost_friends_handler.handle_friend_data(friend_data);
+                new_friends_handler.handle_friend_data(friend_data);
+            } else if (friend_data.friends_lost=='no' 
+                && (friend_data.friends_obtained=='yes' || friend_data.friends_obtained=='all_new')
+                ) 
+            {
+                var new_friends_handler = mediator.create_object(NewFriendsHandler);
+                new_friends_handler.handle_friend_data(friend_data);
+            } else if (
+                (friend_data.friends_lost=='yes' || friend_data.friends_lost=='all_lost') 
+                && friend_data.friends_obtained=='no'
+                ) 
+            {
+                var lost_friends_handler = mediator.create_object(LostFriendsHandler);
+                lost_friends_handler.handle_friend_data(friend_data);
+            }
+        }
+    }
 
 }
